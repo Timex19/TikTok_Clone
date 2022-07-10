a0e3eea242b8594f2a7d32843800ab0fc9214ff0
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2022-07-10 23:32:54 +0100

    Adding new features and improving code

--- utils/queries.ts
+++ utils/queries.ts
@@ -29,34 +29,34 @@ export const allPostsQuery = () => {
     return query;
 };
   
-  export const postDetailQuery = (postId: string | string[]) => {
+export const postDetailQuery = (postId: string | string[] | any) => {
     const query = `*[_type == "post" && _id == '${postId}']{
+      _id,
+       caption,
+         video{
+          asset->{
+            _id,
+            url
+          }
+        },
+        userId,
+      postedBy->{
         _id,
-        caption,
-            video{
-            asset->{
-                _id,
-                url
-            }
-            },
-            userId,
+        userName,
+        image
+      },
+       likes,
+      comments[]{
+        comment,
+        _key,
         postedBy->{
-            _id,
-            userName,
-            image
-        },
-        likes,
-        comments[]{
-            comment,
-            _key,
-            postedBy->{
-            _ref,
-            _id,
-        },
-        }
-        }`;
+          _ref,
+        _id,
+      },
+      }
+    }`;
     return query;
-};
+  };
   
   export const searchPostsQuery = (searchTerm: string | string[]) => {
     const query = `*[_type == "post" && caption match '${searchTerm}*' || topic match '${searchTerm}*'] {
@@ -88,7 +88,7 @@ export const allPostsQuery = () => {
     return query;
 };
   
-    export const singleUserQuery = (userId: string | string[]) => {
+    export const singleUserQuery = (userId: string | string[] | any) => {
         const query = `*[_type == "user" && _id == '${userId}']`;
     
         return query;
@@ -100,7 +100,7 @@ export const allPostsQuery = () => {
         return query;
 };
   
-  export const userCreatedPostsQuery = (userId: string | string[]) => {
+  export const userCreatedPostsQuery = (userId: string | string[] | any) => {
     const query = `*[ _type == 'post' && userId == '${userId}'] | order(_createdAt desc){
         _id,
         caption,
@@ -131,7 +131,7 @@ export const allPostsQuery = () => {
     return query;
 };
   
-  export const userLikedPostsQuery = (userId: string | string[]) => {
+  export const userLikedPostsQuery = (userId: string | string[] | any) => {
     const query = `*[_type == 'post' && '${userId}' in likes[]._ref ] | order(_createdAt desc) {
         _id,
         caption,
