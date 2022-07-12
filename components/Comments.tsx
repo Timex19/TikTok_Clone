e36e95c8136105fdcc1a3c7ea8eb07b73f16c616
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2022-07-12 14:20:50 +0100

    Final code refactor

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -68,11 +68,12 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
           <NoResults text='No Comments Yet! Be First to do add the comment.' />
         )}
       </div>
-     {userProfile && <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
+      {userProfile && <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
         <form onSubmit={addComment} className='flex gap-4'>
           <input
+            type='text'
             value={comment}
-            onChange={(e) => setComment(e.target.value.trim())}
+            onChange={(e) => setComment(e.target.value)}
             className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
             placeholder='Add comment..'
           />
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { FaCloudUploadAlt } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { FaCloudUploadAlt } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
 import axios from 'axios';
 import { SanityAssetDocument } from '@sanity/client';
